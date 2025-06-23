35a15f8ddeb406ca5b3fbdf0951003e721f3a7d0
Mahammad Shariar Alam <mahammadshariaralam@gmail.com>
2025-06-23 23:45:49 +0600

    g

---
+++ src/article/article.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { UpdateArticleDto } from './dto/update-article.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class ArticleService {
+  constructor(private readonly prisma: PrismaService) {}
+  create(createArticleDto: CreateArticleDto) {
+    return 'This action adds a new article';
+  }
+
+  async findAll() {
+    const data = await this.prisma.article.findMany({
+      where: { published: true },
+    });
+    return { message: `This action returns all article` };
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} article`;
+  }
+
+  update(id: number, updateArticleDto: UpdateArticleDto) {
+    return `This action updates a #${id} article`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} article`;
+  }
+}
